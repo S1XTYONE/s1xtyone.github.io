4df0348b475eb2affc4a10149b88e6f6b957310a
islam <racer3229@gmail.com>
2018-02-13 23:49:49 +0600

    login
    login

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php 
+if(isset($_POST['submit']))
+{
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    
+    $file = fopen("data.txt", "w+") or die ("file not found");
+    
+    $s = $email . ";" . $pass . '\n';
+    fputs($file, $s) or die ("Ошибка!");
+    
+    fclose($file);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Вход</title>
+</head>
+<body>
+    <form action="#" method="post">
+        email: <input type="email" name="email"><br>
+        pass: <input type="password" name="pass">
+    </form>
+</body>
+</html>
